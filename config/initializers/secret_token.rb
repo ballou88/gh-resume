1bebd6898556a54380c86c5c7e8f51fa7d9e6574
Mike Ballou <mikeballou@gmail.com>
2013-03-26 12:43:24 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sendgrid::Application.config.secret_token = 'feb29659a4d32e0cf0e13cdca83bb98b32714088ac902f10087a15b686cbb6f4bb344cef9d7700f97b6b35c08959a04caa38d0e4a4ecd9c4913b9db73259a025'
