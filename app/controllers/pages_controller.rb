6c8b57f8c164e02faf9b27919d1c75a3e9bcbfe3
Mike Ballou <mikeballou@gmail.com>
2013-03-26 23:52:03 -0700

    added delayed job, and made emails use it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
     @user = Github.user_information(params[:search])
   end
   def mail
-    ResumeMailer.resume_email(params[:email].first, params[:username]).deliver
+    ResumeMailer.delay.resume_email(params[:email].first, params[:username])
     redirect_to root_path, notice: "Email Sent."
   end
 end
